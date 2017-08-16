97a39e30d1e84f73736225e594e991e86916ed71
zhangcunli <zhangcunli8499@gmail.com>
2017-08-16 15:04:44 +0800

    test

--- zan-extension/include/zanLog.h
+++ zan-extension/include/zanLog.h
@@ -15,6 +15,7 @@
   | Author: Zan Group   <zan@zanphp.io>                                  |
   +----------------------------------------------------------------------+
 */
+
 #ifndef _ZAN_ZANLOG_H_
 #define _ZAN_ZANLOG_H_
 
